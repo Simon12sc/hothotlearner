1b6b9e419405922af41df303ef3c12ce2281c7f7
Simon12sc <simonchaudhary147@gmail.com>
2023-01-25 02:02:46 +0545

    add dotenv config

--- configs/postgre.config.js
+++ configs/postgre.config.js
@@ -1,4 +1,6 @@
 import { Sequelize } from "sequelize";
+import dotenv from "dotenv"
+dotenv.config({path:"configs/.env"});
 
 const db=new Sequelize(process.env.DATABASE,process.env.DB_USER,process.env.DB_PASSWORD,{
     host:process.env.DB_HOST,
